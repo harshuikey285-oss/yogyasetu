05e3cae2dd73a2ac28847eaed648fd12e82762a5
harshuikey285-oss <harshuikey285@gmail.com>
2026-08-05 20:24:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Welcome to YogyaSetu 2.0");
+document.querySelectorAll(".category-card").forEach(card=>{
+
+card.addEventListener("click",()=>{
+
+let category=card.querySelector("h3").innerText;
+
+window.location.href="schemes.html?category="+category;
+
+});
+
+});
